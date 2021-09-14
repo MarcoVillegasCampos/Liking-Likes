9375175e6f59c8be7e70d042c9d3dadc293b99cb
Your Name <you@example.com>
2021-09-14 01:01:12 -0600

    adding files

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+function likes(element){
+    
+    let liked=document.getElementById("number").innerHTML;
+       Number(liked);
+      
+       for(let i=0; i> liked.length; i++){
+        liked.innerHTML=liked[i];
+        
+
+        
+
+
+       }
+       
+
+
+
+
+    
+    
+
+
+   
+
+
+
+
+
+
+    
+
+
+
+
+
+
+}
